9a9bfcf9c6909c93bdf0dee2bacdb92b7cfaba93
geng <snggeng@gmail.com>
2018-04-24 17:51:36 -0400

    added semantic-ui and successfully render VR along with web components

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SceneContainer from './Scene'
+import { Grid } from 'semantic-ui-react'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {color: 'red'};
+  }
+
+  changeColor() {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    this.setState({
+      color: colors[Math.floor(Math.random() * colors.length)]
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +24,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Grid divided='vertically'>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <SceneContainer className="flex"/>
+          </Grid.Column>
+          <Grid.Column>
+            <div className="flex">IN THE MIDDLE</div>
+          </Grid.Column>
+        </Grid.Row>
+        </Grid>
       </div>
     );
   }
--- react-client/src/Scene.js
+++ react-client/src/Scene.js
@@ -1,12 +1,12 @@
 import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
-// import 'babel-polyfill';
+import 'babel-polyfill';
 import {Entity, Scene} from 'aframe-react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 
-class Scene extends React.Component {
+class SceneContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {color: 'red'};
@@ -21,37 +21,40 @@ class Scene extends React.Component {
 
   render () {
     return (
-      <Scene>
-        <a-assets>
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
-        </a-assets>
+        <div style={{height: '500px'}}>
+            <h1>VR Scene</h1>
+            <Scene embedded={true}>
+                <a-assets>
+                <img crossOrigin="anonymous" id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
+                <img crossOrigin="anonymous" id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+                </a-assets>
 
-        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
-        <Entity primitive="a-light" type="ambient" color="#445451"/>
-        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+                <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+                <Entity primitive="a-light" type="ambient" color="#445451"/>
+                <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
+                <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+                <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
+                <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
-        <Entity id="box"
-          geometry={{primitive: 'box'}}
-          material={{color: this.state.color, opacity: 0.6}}
-          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 1, z: -3}}
-          events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                  material={{color: '#24CAFF'}}/>
-        </Entity>
+                <Entity id="box"
+                geometry={{primitive: 'box'}}
+                material={{color: this.state.color, opacity: 0.6}}
+                // animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
+                // animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
+                position={{x: 0, y: 1, z: -3}}
+                events={{click: this.changeColor.bind(this)}}>
+                {/* <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+                        geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
+                        material={{color: '#24CAFF'}}/> */}
+                </Entity> 
 
-        <Entity primitive="a-camera">
-          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
-        </Entity>
-      </Scene>
+                {/* <Entity primitive="a-camera">
+                <Entity primitive="a-cursor" animation__click={{startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+                </Entity> */}
+            </Scene>
+        </div>
     );
   }
 }
 
-export default Scene
+export default SceneContainer
